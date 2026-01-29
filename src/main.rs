ea9c797683c6de306f75fe46da6af9e1fc08fcf0
LukoOG <emmanueladesipe01@gmail.com>
2026-01-29 21:01:10 +0100

    upgraded first_word function

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,32 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin()
-    .read_line(&mut guess)
-    .expect("failed to read line");
+        .read_line(&mut guess)
+        .expect("failed to read line");
 
     println!("You guessed {guess}");
     println!("{}", five());
     for number in (1..10).rev() {
         println!("The number is {}", number)
-    };
+    }
+
+    let example = "Helpokklo World".to_string();
+
+    println!("{}", {first_word(&example)});
 }
 
-fn five()->u16{
+fn five() -> u16 {
     5
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
